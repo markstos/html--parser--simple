21e06df2749779713d5af4553d30741dba5e4707
markstos <mark@stosberg.com>
2009-04-10 22:25:28 -0400

    Get rid of Exporter code. This is an OO module. There is nothing to export.

--- lib/HTML/Parser/Simple.pm
+++ lib/HTML/Parser/Simple.pm
@@ -11,31 +11,9 @@ use warnings;
 
 require 5.005_62;
 
-require Exporter;
-
 use Carp;
 use File::Spec;
-
 use Tree::Simple;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use HTML::Parser::Simple ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-
-);
 
 our $VERSION = '1.02';
 
