28226e538173b863fcda710ca9868223d364e2d7
markstos <mark@stosberg.com>
2009-04-11 16:21:46 -0400

    More documentation updates for HTML::Parser::Simple::Tree.

--- lib/HTML/Parser/Simple/Tree.pm
+++ lib/HTML/Parser/Simple/Tree.pm
@@ -6,15 +6,35 @@ use Carp;
 use strict;
 use warnings;
 
-=head2 astring
+=head1 NAME
 
-return the attributes for a tag as a string
+C<HTML::Parser::Simple::Tree> - represent HTML as a Tree::Simple tree
+
+=head1 Synopsis
+
+Generally this module is used through L<HTML::Parser::Simple>, not directly. 
+
+=head1 METHODS
+
+This class inherits all the methods from L<Tree::Simple> and adds the following
+new ones which apply to HTML trees. 
+
+=head2 get_attr_string
+
+ my $attr_str = $self->get_attr_string. 
+
+Return the attributes for a tag as a string.  It assumes that the attribute
+string was previously set through a constructor:
+
+ HTML::Parser::Simple::Tree->new( {
+    attributes => ' height="20" width="20"',
+    # ...
+ }, $parent);
 
 =cut
 
 sub get_attr_string { 
     my $self = shift;
-
     return $self->getNodeValue->{attributes}; 
 } 
 
@@ -27,11 +47,10 @@ If you have a start tag, this will return a hash ref with the attribute names as
 
 If you pass in an attribute name, it will return the value for just that attribute.
 
-
 =cut 
 
-# Should also return false if the token is not a start tag, but how/
-
+# Should also return false if the token is not a start tag, but how?
+# Or perhaps only start tags become nodes? 
 sub get_attr {
     my $self = shift;
     my $key = shift;    
@@ -52,13 +71,19 @@ sub get_attr {
 
 }
 
-# -----------------------------------------------
+=head2 parse_attributes
 
+ $attr_href = $self->parse_attributes($attribute_string);
+
+Parses a string of HTML attributes and returns the result as a hash ref, or
+dies if the string is a valid attribute string. Attribute values may be quoted
+with double quotes, single quotes, no quotes if there are no spaces in the value. 
+
+=cut
 
 our $quote_re  = qr{^([a-zA-Z0-9_-]+)\s*=\s*["]([^"]+)["]\s*(.*)$}o; # regular quotes
 our $squote_re = qr{^([a-zA-Z0-9_-]+)\s*=\s*[']([^']+)[']\s*(.*)$}o; # single quotes
 our $uquote_re = qr{^([a-zA-Z0-9_-]+)\s*=\s*([^\s'"]+)\s*(.*)$}o; # unquoted
-
 sub parse_attributes {
     my $self = shift;  
     my $astring = shift;
@@ -92,6 +117,32 @@ sub parse_attributes {
     return \%attrs;
 }
 
+=head1 Required Modules
+
+=over 4
+
+=item Carp
+
+=item Tree::Simple
+
+=back
+
+=head1 Author
+
+C<HTML::Parser::Simple::Tree> was written by Mark Stosberg I<E<lt>mark@summersault.comE<gt>> in 2009.
+
+Home page: http://mark.stosberg.com/
+
+=head1 Copyright
+
+Copyright (c) 2009 Mark Stosberg.
+
+	All Programs of mine are 'OSI Certified Open Source Software';
+	you can redistribute them and/or modify them under the terms of
+	The Artistic License, a copy of which is available at:
+	http://www.opensource.org/licenses/index.html
+
+=cut
 
 
 
