8baaddbbeacc6f0cacb21d55407d5b99fbb04356
Mark Stosberg <mark@stosberg.com>
2009-10-03 17:25:12 -0400

    Convert tabs to spaces, reformatt docs.

--- lib/HTML/Parser/Simple.pm
+++ lib/HTML/Parser/Simple.pm
@@ -10,27 +10,27 @@ our $VERSION = '1.02';
 
 # Encapsulated class data.
 {
-	my  %_attr_data  = (
-	 _xhtml      => 0,
-	);
+    my  %_attr_data  = (
+     _xhtml      => 0,
+    );
 
-	sub _default_for {
-		my($self, $attr_name) = @_;
-		$_attr_data{$attr_name};
-	}
+    sub _default_for {
+        my($self, $attr_name) = @_;
+        $_attr_data{$attr_name};
+    }
 
-	sub _standard_keys {
-		keys %_attr_data;
-	}
+    sub _standard_keys {
+        keys %_attr_data;
+    }
 }
 
 sub handle_doctype {
-	my($self, $s) = @_;
-	$self -> handle_content($s);
+    my($self, $s) = @_;
+    $self -> handle_content($s);
 }
 
 sub handle_start_tag {
-	my($self, $tag_name, $attributes, $unary) = @_;
+    my($self, $tag_name, $attributes, $unary) = @_;
     # Sub-class to do something interesting.
     return 1;
 }
@@ -43,36 +43,36 @@ sub new {
 
 sub init {
     my $self = shift;
-	my $arg  = shift;
+    my $arg  = shift;
 
-    $self->{_stack}        = []; # list of start targs in the order they are found.
+    $self->{_stack}      = []; # list of start targs in the order they are found.
     $self->{_in_content} = 0 ; # a boolean. Tracks whether the current point in parsing is in content or not. 
 
-	for my $attr_name ($self -> _standard_keys() ) {
-		my($arg_name) = $attr_name =~ /^_(.*)/;
+    for my $attr_name ($self -> _standard_keys() ) {
+        my($arg_name) = $attr_name =~ /^_(.*)/;
 
-		if (exists($$arg{$arg_name}) ) {
-			$$self{$attr_name} = $$arg{$arg_name};
-		}
-		else {
-			$$self{$attr_name} = $self -> _default_for($attr_name);
-		}
-	}
+        if (exists($$arg{$arg_name}) ) {
+            $$self{$attr_name} = $$arg{$arg_name};
+        }
+        else {
+            $$self{$attr_name} = $self -> _default_for($attr_name);
+        }
+    }
 
     # Block Elements - HTML 4.01
-	$$self{'_block'} = _make_map(qw{ address applet blockquote button center dd del dir div dl dt fieldset form frameset hr iframe ins isindex li map menu noframes noscript object ol p pre script table tbody td tfoot th thead tr ul});
+    $$self{'_block'} = _make_map(qw{ address applet blockquote button center dd del dir div dl dt fieldset form frameset hr iframe ins isindex li map menu noframes noscript object ol p pre script table tbody td tfoot th thead tr ul});
 
     # Elements that you can, intentionally, leave open
     # (and which close themselves)
-	$$self{'_close_self'} = _make_map(qw{ colgroup dd dt li option p td tfoot th thead tr });
+    $$self{'_close_self'} = _make_map(qw{ colgroup dd dt li option p td tfoot th thead tr });
 
     # Empty Elements - HTML 4.01
-	$$self{'_empty'} = _make_map(qw{ area base basefont br col embed frame hr img input isindex link meta param wbr });
+    $$self{'_empty'} = _make_map(qw{ area base basefont br col embed frame hr img input isindex link meta param wbr });
 
     # Inline Elements - HTML 4.01
-	$$self{'_inline'} = _make_map(qw{ a abbr acronym applet b basefont bdo big br button cite code del dfn em font i iframe img input ins kbd label map object q s samp script select small span strike strong sub sup textarea tt u var });
+    $$self{'_inline'} = _make_map(qw{ a abbr acronym applet b basefont bdo big br button cite code del dfn em font i iframe img input ins kbd label map object q s samp script select small span strike strong sub sup textarea tt u var });
 
-	$$self{'_known_tag'} = {%{$$self{'_block'} }, %{$$self{'_close_self'} }, %{$$self{'_empty'} }, %{$$self{'_inline'} } };
+    $$self{'_known_tag'} = {%{$$self{'_block'} }, %{$$self{'_close_self'} }, %{$$self{'_empty'} }, %{$$self{'_inline'} } };
 
     # XHTML allows "xml:lang" in the <html> tag, but HTML does not allow colons in attribute names.
     my $maybe_colon  = $self ->{'_xhtml'} ? ':' : '';
@@ -88,146 +88,146 @@ sub init {
     $$self{'_uquote_re'} = qr{^($attr_name_re)\s*=\s*([^\s'"]+)\s*(.*)$}so;    # unquoted
     $$self{'_bool_re'}   = qr{^($attr_name_re)\s*(.*)$}so;                     # a boolean, like "checked"
 
-	return $self;
+    return $self;
 
-}	# End of new.
+}   # End of new.
 
 # -----------------------------------------------
 
 sub parse {
-	my($self, $html) = @_;
-	my($original)    = $html;
-	my(%special)     = (
-	 script => 1,
-	 style  => 1,
-	);
+    my($self, $html) = @_;
+    my($original)    = $html;
+    my(%special)     = (
+     script => 1,
+     style  => 1,
+    );
 
     my $offset; # position relative to the beginning of the chunk
     my $s; # current snippet of HTML we are testing and parsing
 
     # Use $stack as a shorthand. Since they are both references, as $stack is updated, our internal stack will be too.
-	my $stack = $self->{_stack};
+    my $stack = $self->{_stack};
 
-	for (; $html;) {
-		$self->{_in_content} = 1;
+    for (; $html;) {
+        $self->{_in_content} = 1;
 
-		# Make sure we're not in a script or style element.
+        # Make sure we're not in a script or style element.
 
-		if (! $$stack[$#$stack] || ! $special{$$stack[$#$stack]}) {
-			# Rearrange order of testing so rarer possiblilites are further down.
-			# Is it an end tag?
+        if (! $$stack[$#$stack] || ! $special{$$stack[$#$stack]}) {
+            # Rearrange order of testing so rarer possiblilites are further down.
+            # Is it an end tag?
 
-			$s = substr($html, 0, 2);
+            $s = substr($html, 0, 2);
 
-			if ($s eq '</') {
-				if ($html =~ /^(<\/(\w+)[^>]*>)/o) {
+            if ($s eq '</') {
+                if ($html =~ /^(<\/(\w+)[^>]*>)/o) {
                     my ($whole_tag,$tag_name) = ($1,$2);
-					substr($html, 0, length $whole_tag) = '';
-					$self->{_in_content}                 = 0;
+                    substr($html, 0, length $whole_tag) = '';
+                    $self->{_in_content}                 = 0;
 
-					$self -> parse_end_tag($tag_name, $stack);
-				}
-			}
+                    $self -> parse_end_tag($tag_name, $stack);
+                }
+            }
 
-			# Is it a start tag?
+            # Is it a start tag?
 
-			if ($self->{_in_content}) {
-				if (substr($html, 0, 1) eq '<') {
-					if ($html =~ $$self{'_tag_with_attribute'}) {
+            if ($self->{_in_content}) {
+                if (substr($html, 0, 1) eq '<') {
+                    if ($html =~ $$self{'_tag_with_attribute'}) {
                         my ($orig_text,$tag_name,$attr_string,$unary) = ($1,$2,$3,$4);
-						substr($html, 0, length $orig_text) = '';
-						$self->{_in_content}                 = 0;
+                        substr($html, 0, length $orig_text) = '';
+                        $self->{_in_content}                 = 0;
 
-						$self -> parse_start_tag($tag_name, $attr_string, $unary, $stack);
-					}
-				}
-			}
+                        $self -> parse_start_tag($tag_name, $attr_string, $unary, $stack);
+                    }
+                }
+            }
 
-			# Is it a comment?
+            # Is it a comment?
 
-			if ($self->{_in_content}) {
-				$s = substr($html, 0, 4);
+            if ($self->{_in_content}) {
+                $s = substr($html, 0, 4);
 
-				if ($s eq '<!--') {
-					$offset = index($html, '-->');
+                if ($s eq '<!--') {
+                    $offset = index($html, '-->');
 
-					if ($offset >= 0) {
-						$self -> handle_comment(substr($html, 0, ($offset + 3) ) );
+                    if ($offset >= 0) {
+                        $self -> handle_comment(substr($html, 0, ($offset + 3) ) );
 
-						substr($html, 0, $offset + 3) = '';
-						$self->{_in_content}                   = 0;
-					}
-				}
-			}
+                        substr($html, 0, $offset + 3) = '';
+                        $self->{_in_content}                   = 0;
+                    }
+                }
+            }
 
-			# Is it a doctype?
+            # Is it a doctype?
 
-			if ($self->{_in_content}) {
-				$s = substr($html, 0, 9);
+            if ($self->{_in_content}) {
+                $s = substr($html, 0, 9);
 
-				if ($s eq '<!DOCTYPE') {
-					$offset = index($html, '>');
+                if ($s eq '<!DOCTYPE') {
+                    $offset = index($html, '>');
 
-					if ($offset >= 0) {
-						$self -> handle_doctype(substr($html, 0, ($offset + 1) ) );
+                    if ($offset >= 0) {
+                        $self -> handle_doctype(substr($html, 0, ($offset + 1) ) );
 
-						substr($html, 0, $offset + 1) = '';
-						$self->{_in_content}                   = 0;
-					}
-				}
-			}
+                        substr($html, 0, $offset + 1) = '';
+                        $self->{_in_content}                   = 0;
+                    }
+                }
+            }
 
-			# Is is an XML declaration?
+            # Is is an XML declaration?
 
-			if ($self ->{'_xhtml'} && $self->{_in_content}) {
-				$s = substr($html, 0, 5);
+            if ($self ->{'_xhtml'} && $self->{_in_content}) {
+                $s = substr($html, 0, 5);
 
-				if ($s eq '<?xml') {
-					$offset = index($html, '?>');
+                if ($s eq '<?xml') {
+                    $offset = index($html, '?>');
 
-					if ($offset >= 0) {
+                    if ($offset >= 0) {
                         my $xml = substr($html, 0, ($offset + 2) );
-						$self -> handle_xml_declaration($xml);
-						substr($html, 0, $offset + 2) = '';
-						$self->{_in_content}                   = 0;
-					}
-				}
-			}
+                        $self -> handle_xml_declaration($xml);
+                        substr($html, 0, $offset + 2) = '';
+                        $self->{_in_content}                   = 0;
+                    }
+                }
+            }
 
-			if ($self->{_in_content}) {
-				$offset = index($html, '<');
+            if ($self->{_in_content}) {
+                $offset = index($html, '<');
 
-				if ($offset < 0) {
-					$self -> handle_content($html);
+                if ($offset < 0) {
+                    $self -> handle_content($html);
 
-					$html = '';
-				}
-				else {
-					$self -> handle_content(substr($html, 0, $offset) );
-					substr($html, 0, $offset) = '';
-				}
-			}
-		}
-		else {
-			my($re) = "(.*)<\/$$stack[$#$stack]\[^>]*>";
+                    $html = '';
+                }
+                else {
+                    $self -> handle_content(substr($html, 0, $offset) );
+                    substr($html, 0, $offset) = '';
+                }
+            }
+        }
+        else {
+            my($re) = "(.*)<\/$$stack[$#$stack]\[^>]*>";
 
-			if ($html =~ /$re/s) {
-				my($text) = $1;
-				$text     =~ s/<!--(.*?)-->/$1/g;
-				$text     =~ s/<!\[CDATA]\[(.*?)]]>/$1/g;
+            if ($html =~ /$re/s) {
+                my($text) = $1;
+                $text     =~ s/<!--(.*?)-->/$1/g;
+                $text     =~ s/<!\[CDATA]\[(.*?)]]>/$1/g;
 
-				$self -> handle_content($text);
-			}
+                $self -> handle_content($text);
+            }
 
-			$self -> parse_end_tag($$stack[$#$stack], $stack);
-		}
+            $self -> parse_end_tag($$stack[$#$stack], $stack);
+        }
 
-		if ($html eq $original) {
+        if ($html eq $original) {
             $self->handle_parse_error($html)
-		}
+        }
 
-		$original = $html;
-	}
+        $original = $html;
+    }
 
 
     # For compatibility with HTML::Parser;
@@ -308,8 +308,8 @@ sub parse_attributes {
 sub eof {
     my $self = shift; 
     
-	# Clean up any remaining tags.
-	$self -> parse_end_tag('', $self->{_stack} );
+    # Clean up any remaining tags.
+    $self -> parse_end_tag('', $self->{_stack} );
 
     return $self;
 }
@@ -317,43 +317,43 @@ sub eof {
 
 
 sub parse_end_tag {
-	my($self, $tag_name, $stack) = @_;
+    my($self, $tag_name, $stack) = @_;
 
     unless ($self->case_sensitive) {
         $tag_name = lc $tag_name;
     }
 
-	# Find the closest opened tag of the same name.
+    # Find the closest opened tag of the same name.
     my $lc_tag_name = lc $tag_name;
 
     # What's the furthest point up the stack we should travel?
-	my $pos;
+    my $pos;
 
     # If we closing a specific tag, see how far up the stack it is
-	if ($tag_name) {
+    if ($tag_name) {
         STACKCHECK: for ($pos = $#$stack; $pos >= 0; $pos--) {
-			if ($$stack[$pos] eq $lc_tag_name) {
-				last STACKCHECK;
-			}
-		}
-	}
+            if ($$stack[$pos] eq $lc_tag_name) {
+                last STACKCHECK;
+            }
+        }
+    }
     # Otherwise, we are closing everthing.
-	else {
-		$pos = 0;
-	}
+    else {
+        $pos = 0;
+    }
 
-	if ($pos >= 0) {
-		# Close all the open tags, up the stack.
-		my $count = 0;
+    if ($pos >= 0) {
+        # Close all the open tags, up the stack.
+        my $count = 0;
 
-		for (my $i = $#$stack; $i >= $pos; $i--) {
-			$count++;
-			$self -> handle_end_tag($$stack[$i]);
-		}
+        for (my $i = $#$stack; $i >= $pos; $i--) {
+            $count++;
+            $self -> handle_end_tag($$stack[$i]);
+        }
 
-		# Remove the open elements from the stack.
+        # Remove the open elements from the stack.
         splice @$stack, -$count;
-	}
+    }
 }
 
 sub handle_parse_error {
@@ -362,7 +362,7 @@ sub handle_parse_error {
 }
 
 sub parse_start_tag {
-	my($self, $tag_name, $attributes_str, $unary, $stack) = @_;
+    my($self, $tag_name, $attributes_str, $unary, $stack) = @_;
 
     my ($attr_href,$attr_seq) = $self->parse_attributes($attributes_str);
 
@@ -378,20 +378,20 @@ sub parse_start_tag {
     # internal hash lookups, not external display.
     my $lc_tag_name = lc $tag_name;
 
-	if ($$self{'_block'}{$lc_tag_name}) {
-		for (; $#$stack >= 0 && $$self{'_inline'}{$$stack[$#$stack]};) {
-			$self -> parse_end_tag($$stack[$#$stack], $stack);
-		}
-	}
+    if ($$self{'_block'}{$lc_tag_name}) {
+        for (; $#$stack >= 0 && $$self{'_inline'}{$$stack[$#$stack]};) {
+            $self -> parse_end_tag($$stack[$#$stack], $stack);
+        }
+    }
 
-	if ($$self{'_close_self'}{$lc_tag_name} && ($$stack[$#$stack] eq $lc_tag_name) ) {
-		$self -> parse_end_tag($tag_name, $stack);
-	}
+    if ($$self{'_close_self'}{$lc_tag_name} && ($$stack[$#$stack] eq $lc_tag_name) ) {
+        $self -> parse_end_tag($tag_name, $stack);
+    }
 
-    #	$unary = $$self{'_empty'}{$lc_tag_name} || $unary;
-	if (not ($$self{'_empty'}{$lc_tag_name} || $unary)) {
-		push @$stack, $lc_tag_name;
-	}
+    #   $unary = $$self{'_empty'}{$lc_tag_name} || $unary;
+    if (not ($$self{'_empty'}{$lc_tag_name} || $unary)) {
+        push @$stack, $lc_tag_name;
+    }
 
     # XXX Fake orig text
     my $maybe_slash = $unary ? ' /' : '';
@@ -402,7 +402,7 @@ sub parse_start_tag {
         $attr_href->{'/'} = 1,
     }
 
-	$self ->start($tag_name, $attr_href, $attr_seq, $orig_text);
+    $self ->start($tag_name, $attr_href, $attr_seq, $orig_text);
 }
 sub start { die "must be defined in subclass"  }
 
@@ -414,7 +414,7 @@ sub text { die "most be defined in subclass" }
 
 
 sub handle_end_tag {
-	my($self, $tag_name) = @_;
+    my($self, $tag_name) = @_;
 
     # XXX fake the $orig_text
     my $orig_text = "</$tag_name>";
@@ -443,15 +443,6 @@ sub attr_encoded {
     # XXX right now this defined for compatibility,
     # It doesn't do anything yet. 
 }
-
-=head2 case_sensitive() 
-
- $bool =  $p->case_sensitive($bool);
-
-By default, tagnames and attribute names are down-cased.  Enabling this
-attribute leaves them as found in the HTML source document.
-
-=cut
 
 sub case_sensitive {
     my $self = shift;
@@ -482,8 +473,8 @@ C<HTML::Parser::Simple> - Parse nice HTML files without needing a compiler
 
 =head1 Synopsis
 
-	use HTML::Parser::Simple;
-	my $p = HTML::Parser::Simple->new;
+    use HTML::Parser::Simple;
+    my $p = HTML::Parser::Simple->new;
 
    # Parse document text chunk by chunk
    $p->parse($chunk1);
@@ -511,14 +502,14 @@ Here are the names and signatures of the handlers you can define in your
 subclass:
 
  sub handle_start_tag {
- 	my($self, $tag_name, $attribute_string, $unary) = @_;
+    my($self, $tag_name, $attribute_string, $unary) = @_;
      # Sub-class to do something interesting.
      # to parse the attribute string, see parse_attributes()
      return 1;
  }
 
  sub handle_end_tag {
- 	my($self, $tag_name) = @_;
+    my($self, $tag_name) = @_;
      # Sub-class to do something interesting.
      return 1;
  }
@@ -531,50 +522,42 @@ subclass:
  }
 
  sub handle_comment {
- 	my($self, $comment) = @_;
- 	$self -> handle_content($comment);
+    my($self, $comment) = @_;
+    $self -> handle_content($comment);
  }
 
  sub handle_doctype {
- 	my($self, $doctype) = @_;
- 	$self -> handle_content($doctype);
+    my($self, $doctype) = @_;
+    $self -> handle_content($doctype);
  }
 
  sub handle_xml_declaration {
- 	my($self, $s) = @_;
- 	$self->handle_content($s);
+    my($self, $s) = @_;
+    $self->handle_content($s);
  }
 
 
-For examples, you can see the two sub-classes which are included in this distribution:
+For an example, you can see a subclass which is included in this distribution:
 
 L<HTML::Parser::Simple::Tree> - stores the parsed document in an
 L<Tree::Simple> object.  This allows for new ways to access the data, at the
 expense of storing the entire HTML document in memory as a collection of
 Tree::Simple objects.
 
-L<HTML::Parser::Simple::Compat> - is designed for some compatibility with
-HTML::Parser, allowing you to eliminate the compiler requirement that
-HTML::Parser brings it's use of XS and C. The trade-off is that the pure Perl
-approach is of course slower, and compatibility might not be perfect.
-
-
 =head1 Methods
 
 =head2 new()
 
- $parser = HTML::Parser::Simple->new;
+ $p = HTML::Parser::Simple->new;
 
 new(...) returns an object of type C<HTML::Parser::Simple>.
 It takes a hashref of the following options.
 
 =over 4
 
-=item xhtml(0|1)
+=item $p->xhtml(0|1)
 
-This takes either a 0 or a 1.
-
-Boolean to optionally enable the following XHTML feature
+Boolean to optionally enable the following XHTML feature:
 
 =over 4
 
@@ -585,8 +568,6 @@ E.g.: <?xml version="1.0" standalone='yes'?>.
 =item Accept attribute names containing the ':' char
 
 E.g.: <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">.
-
-=back
 
 =back
 
@@ -626,7 +607,6 @@ a file handle that is not in binary mode.
 If a filename is passed in, then parse_file() will open the file in
 binary mode.
 
-
 =head2 $p->parse_attributes($attr_string)
 
  ($attr_href,$attr_seq) = $p->parse_attributes($attr_string);
@@ -639,7 +619,7 @@ with double quotes, single quotes, no quotes if there are no spaces in the value
 In list context, second return value is an arrayref which contains the keys in
 the order in which they were found.
 
-=head2 eof
+=head2 $p->eof
 
 Signals the end of the HTML document. All remaining HTML tags on the internal 
 parse stack will now be closed. 
@@ -648,6 +628,15 @@ The return value from eof() is a reference to the parser object.
 
 The behavior of C<< eof () >> in HTML::Parser is more involved. This method
 is not yet fully compatible.
+
+=head2 $->case_sensitive()
+
+ $bool =  $p->case_sensitive($bool);
+
+By default, tagnames and attribute names are down-cased.  Enabling this
+attribute leaves them as found in the HTML source document.
+
+=cut
 
 =back
 
